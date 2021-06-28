d3303f656f5b557df163bce4ec6c006401b70ba4
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-06-28 13:47:11 +0100

    try to run twemproxy as redis ambassador
    - Cannot figure out how to actually access proxy

--- ambassadors/foo.sh
+++ ambassadors/foo.sh
@@ -9,6 +9,12 @@ minikube start
 # Load images from local docker instance
 minikube cache add redis:6.2.3-alpine
 
+# Load images from local docker instance (after upgrade to minikube v1.21)
+minikube image load ganomede/twemproxy
+
+# Set shell to use minikube docker
+eval $(minikube docker-env)
+
 # Show Current cluster status
 kubectl get po -A
 
@@ -20,3 +26,9 @@ kubectl create  -f redis-shards.yml
 
 # Changese
 kubectl apply -f redis-shards.yml
+
+# Create Service
+kubectl create -f redis-service.yml
+
+# Create config map
+kubectl create configmap nutcracker --from-file=nutcracker.yml
